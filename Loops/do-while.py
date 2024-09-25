28563d7f5b0222d8a5b70bc024977423c62886d9
Dominic Oladapo-Tonade <dominicoladapotonade@gmail.com>
2024-09-25 22:25:48 +0100

    Create do-while.py

---
+++ Loops/do-while.py
@@ -0,0 +1,17 @@
+i = 0
+
+#condition for the loop
+condition = True
+
+#execute loop
+do:
+    print(f"I Code python {i}")
+    i += 1
+    if condition:
+        continue do
+
+#loop terminator
+print("Loop terminated.")
+
+
+      
